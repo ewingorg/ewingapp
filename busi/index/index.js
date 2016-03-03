09c9457a8375b57803634a654119cc9de6b5ab69
ewingcode <fiafa5000@163.com>
2016-03-03 16:17:08 +0800

    add

--- busi/index/index.js
+++ busi/index/index.js
@@ -4,53 +4,13 @@ mui.init({
 	swipeBack: true //启用右滑关闭功能
 });
 
-initTemplateKey();
+keytemplate.initTemplateKey(shopId,'index.html');
 initCategorySelect();
 initPullEvent();
 initTapEvent();
 
-function initTemplateKey() {
-	var requestJson = {
-		data: {
-			shopId: shopId,
-			templateName: 'index.html'
-		}
-	};
-	var templateKeyList;
-	ajax.jsonpSyncRequest("web/templateKey.action", requestJson, function(json) {
-		var g = json;
-		templateKeyList = json.result;
-		if (!templateKeyList)
-			return;
 
-		$("div[name^='KEY_']").each(function(e) {
-			var keyContainer = $(this);
-			var expectKey = $(this).attr('name').replace('KEY_', '');
-
-			for (var i = 0; i < templateKeyList.length; i++) {
-				var group = templateKeyList[i];
-				var groupKey = group.groupKey;
-				if (expectKey == groupKey) {
-					keyContainer.loadTemplate($("#keyTemplate"), group.blockList);
-					break;
-				}
-			}
-		}); 
-		
- 		var blockElementList = document.querySelectorAll("[blockid]");
-		for (var i = 0; i < blockElementList.length; i++) {
-			var url = blockElementList[i].getAttribute("blockurl");
-			console.log(url);
-			blockElementList[i].addEventListener('tap', function() {
-				mui.openWindow({
-					id: 'jumpPage',
-					url: this.getAttribute("blockurl")
-				});
-			});
-		}
-	});
-
-}
+	 
 //设置所有事件
 function initTapEvent() {
 
---
+++ busi/js/keytemplate.js
@@ -0,0 +1,44 @@
+if (!this.keytemplate) {
+	var keytemplate = {};
+} 
+
+keytemplate.initTemplateKey = function(shopId,templateName) {
+
+	var requestJson = {
+		data: {
+			shopId: shopId,
+			templateName: templateName
+		}
+	};
+	var templateKeyList;
+	ajax.jsonpSyncRequest("web/templateKey.action", requestJson, function(json) { 
+		templateKeyList = json.result;
+		if (!templateKeyList)
+			return;
+
+		$("div[name^='KEY_']").each(function(e) {
+			var keyContainer = $(this);
+			var expectKey = $(this).attr('name').replace('KEY_', '');
+
+			for (var i = 0; i < templateKeyList.length; i++) {
+				var group = templateKeyList[i];
+				var groupKey = group.groupKey;
+				if (expectKey == groupKey) {
+					keyContainer.loadTemplate($("#keyTemplate"), group.blockList);
+					break;
+				}
+			}
+		}); 
+		
+ 		var blockElementList = document.querySelectorAll("[blockid]");
+		for (var i = 0; i < blockElementList.length; i++) {
+			var url = blockElementList[i].getAttribute("blockurl"); 
+			blockElementList[i].addEventListener('tap', function() {
+				mui.openWindow({
+					id: 'jumpPage',
+					url: this.getAttribute("blockurl")
+				});
+			});
+		}
+	});
+}
