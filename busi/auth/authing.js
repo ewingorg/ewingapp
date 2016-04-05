62dea22380e55cfc12ab8b292dd6e19a633eb8ab
chenxg <chenxg@ucweb.com>
2016-04-05 20:38:19 +0800

    修改微信登陆问题

---
+++ busi/auth/authing.js
@@ -0,0 +1,19 @@
+var code = jQuery.url.param("code");
+var state = jQuery.url.param("state");
+
+
+init();
+
+
+function init(){
+	var requestJson = {
+		data: {
+			code: code,
+			state: state
+		}
+	};
+	ajax.jsonpSyncFetch("apiwx/getAuthCode.action", requestJson, random, "renderFunc");
+}
+
+function renderFunc(json){
+}
